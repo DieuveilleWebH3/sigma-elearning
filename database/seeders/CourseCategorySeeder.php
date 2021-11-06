97a7ceaef303349649734ffaf64f5dafad82e101
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2021-11-06 19:36:26 +0100

    added courses_categories seeders, ...

---
+++ database/seeders/CourseCategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CourseCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        for ($i=1; $i<=25; $i++) {
+
+            DB::table('courses_categories')->insert([
+                'course' => $i,
+                'category' => random_int(1, 5),
+            ]);
+
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CourseSeeder::class,
             ChapterSeeder::class,
+            CourseCategorySeeder::class,
         ]);
     }
 }
