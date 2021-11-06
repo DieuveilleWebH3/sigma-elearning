661cdda41fdbf9a2807fc3fe46bcbf78a0254053
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2021-11-06 18:57:31 +0100

    added chapters seeder, filled chapter table with dummy data, ...

--- database/seeders/ChapterSeeder.php
+++ database/seeders/ChapterSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-class CourseSeeder extends Seeder
+class ChapterSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -18,13 +18,12 @@ class CourseSeeder extends Seeder
         //
         for ($i=0; $i<25; $i++){
 
-            DB::table('courses')->insert([
+            DB::table('chapters')->insert([
                 'title' => Str::random(10),
                 'slug' => \Illuminate\Support\Str::slug('title', '-'),
-                'description' => Str::random(50),
-                'duration' => random_int(1, 54),
-                'level' => random_int(1, 4),
-                'user_id' => 1,
+                'content' => Str::random(150),
+                'course' => random_int(1, 24),
+                'video_url' => Str::random(15),
             ]);
 
         }
