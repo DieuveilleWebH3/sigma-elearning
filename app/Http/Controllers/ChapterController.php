593416d8ab747b5b84e13c98feaff36242d0413b
Dieuveille BOUSSA ELLENGA <d.boussaellenga@h3hitema.fr>
2021-11-11 16:37:03 +0100

    added instructor chapterslist views, ...

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -26,7 +26,39 @@ class ChapterController extends Controller
     //
     public function index()
     {
-        $chapters = Chapter::orderBy('created_at', 'DESC')->get();
+
+        $user = User::find(auth()->user()->id);
+
+        if($user->getUserType() === 'Admin'){
+
+            $chapters = Chapter::orderBy('created_at', 'DESC')->get();
+
+            return view('chapter.list', compact('chapters'));
+        }
+
+        // $chapters = Chapter::orderBy('created_at', 'DESC')->get();
+
+        $courses = Course::where('user_id', '=', $user->id)->get();
+
+        $course_ids = [];
+
+        foreach ($courses as $course) {
+            $course_ids[] = $course->id;
+        }
+
+        $chapters = Chapter::whereIn('course', $course_ids)->get();
+
+        dd([$courses, $course_ids, $chapters]);
+
+        /*
+        $chapters = [];
+
+        foreach ($courses as $course) {
+            // $chapters[] = Chapter::where('course', '=', $course->id)->get();
+            array_push($chapters, Chapter::where('course', '=', $course->id)->get());
+            // $chapters = Chapter::where('course', 'in', $course->chapter)->get();
+        }
+        */
 
         return view('chapter.list', compact('chapters'));
 
